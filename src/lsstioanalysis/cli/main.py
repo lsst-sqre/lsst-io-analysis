763d8d9ae6abc2aad1c27e92856afb2a53f0657a
Jonathan Sick <jsick@lsst.org>
2020-11-10 18:49:11 -0500

    Gather updated metadata via Algolia

--- src/lsstioanalysis/cli/main.py
+++ src/lsstioanalysis/cli/main.py
@@ -6,6 +6,7 @@ import asyncio
 
 import httpx
 import typer
+from algoliasearch.search_client import SearchClient
 
 from lsstioanalysis.ingest import get_ltd_products
 
@@ -13,14 +14,25 @@ app = typer.Typer(help="Inventory and analyze the holdings in lsst.io")
 
 
 @app.command()
-def analyze() -> None:
+def analyze(
+    algolia_id: str = typer.Option("", envvar="ALGOLIA_ID"),
+    algolia_key: str = typer.Option("", envvar="ALGOLIA_KEY"),
+    algolia_index_name: str = typer.Option(
+        "document_dev", envvar="ALGOLIA_INDEX"
+    ),
+) -> None:
     """Run analysis."""
     typer.echo("Running analysis.")
-    asyncio.run(async_analyze())
+    asyncio.run(async_analyze(algolia_id, algolia_key, algolia_index_name))
     typer.echo("Complete.")
 
 
-async def async_analyze():
+async def async_analyze(
+    algolia_id: str, algolia_key: str, algolia_index_name: str
+) -> None:
+    # I couldn't get the async algolia search client to work
+    search_client = SearchClient.create(algolia_id, algolia_key)
     async with httpx.AsyncClient() as client:
-        ltd_products = await get_ltd_products(client)
+        algolia_index = search_client.init_index(algolia_index_name)
+        ltd_products = await get_ltd_products(client, algolia_index)
         print(ltd_products)
--- src/lsstioanalysis/ingest.py
+++ src/lsstioanalysis/ingest.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 __all__ = ["LtdProduct", "get_ltd_products"]
 
 import datetime
-from typing import List, Optional
+from typing import TYPE_CHECKING, List, Optional
 
 import httpx
 from dateutil import parser as datetime_parser
@@ -14,6 +14,9 @@ from pydantic import HttpUrl, validator
 from pydantic.dataclasses import dataclass
 
 from lsstioanalysis.utils import gather_with_concurrency
+
+if TYPE_CHECKING:
+    from algoliasearch.search_index import SearchIndex
 
 
 @dataclass
@@ -24,6 +27,12 @@ class LtdProduct:
     repo_url: HttpUrl
 
     title: str
+
+    description: Optional[str]
+
+    handle: Optional[str]
+
+    series: Optional[str]
 
     editions: int
 
@@ -84,7 +93,7 @@ class LtdProduct:
 
     @classmethod
     async def ingest_product(
-        cls, client: httpx.AsyncClient, url: str
+        cls, client: httpx.AsyncClient, algolia_index: SearchIndex, url: str
     ) -> LtdProduct:
         r = await client.get(url)
         product_data = r.json()
@@ -101,10 +110,35 @@ class LtdProduct:
                 datetime_rebuilt = cls.parse_utc_datetime(date_rebuilt)
                 break
 
+        base_url = product_data["published_url"]
+        if not base_url.endswith("/"):
+            base_url = f"{base_url}/"
+
+        try:
+            algolia_metadata = await AlgoliaMetadata.search_for_url(
+                search_index=algolia_index, base_url=base_url
+            )
+        except ValueError:
+            algolia_metadata = AlgoliaMetadata(
+                series=None,
+                title=None,
+                description=None,
+                handle=None,
+                content_type=None,
+            )
+
+        if algolia_metadata.title:
+            title = algolia_metadata.title
+        else:
+            title = product_data["title"]
+
         ltd_product = cls(
             url=product_data["published_url"],
             repo_url=product_data["doc_repo"],
-            title=product_data["title"],
+            title=title,
+            description=algolia_metadata.description,
+            handle=algolia_metadata.handle,
+            series=algolia_metadata.series,
             editions=edition_count,
             updated=datetime_rebuilt,
         )
@@ -112,11 +146,68 @@ class LtdProduct:
         return ltd_product
 
 
-async def get_ltd_products(client: httpx.AsyncClient) -> List[LtdProduct]:
+async def get_ltd_products(
+    client: httpx.AsyncClient, algolia_index: SearchIndex
+) -> List[LtdProduct]:
     r = await client.get("https://keeper.lsst.codes/products/")
     tasks = [
-        LtdProduct.ingest_product(client, product_url)
+        LtdProduct.ingest_product(client, algolia_index, product_url)
         for product_url in r.json()["products"]
     ]
     results = await gather_with_concurrency(10, *tasks)
     return results
+
+
+@dataclass
+class AlgoliaMetadata:
+    """Additional metadata about an LTD product gathered from Algolia."""
+
+    series: Optional[str]
+    """Document series."""
+
+    title: Optional[str]
+    """Document title."""
+
+    description: Optional[str]
+    """Document description or abstract."""
+
+    handle: Optional[str]
+    """Document handle."""
+
+    content_type: Optional[str]
+    """Content type."""
+
+    @classmethod
+    async def search_for_url(
+        cls, *, search_index: SearchIndex, base_url: str
+    ) -> AlgoliaMetadata:
+        """Create an AlgoliaMetadata from a base documentation url."""
+        request_options = {
+            "filters": f"baseUrl:{cls.escape_facet_value(base_url)}",
+            "attributesToRetrieve": [
+                "handle",
+                "h1",
+                "description",
+                "series",
+                "contentType",
+            ],
+        }
+        results = search_index.search("", request_options)
+        if len(results["hits"]) == 0:
+            raise ValueError(f"Algolia record unavailable for {base_url}")
+
+        hit = results["hits"][0]
+        return cls(
+            title=hit["h1"],
+            description=hit["description"],
+            series=hit["series"],
+            handle=hit["handle"],
+            content_type=hit["contentType"],
+        )
+
+    @staticmethod
+    def escape_facet_value(value: str) -> str:
+        """Escape and quote a facet value for an Algolia search."""
+        value = value.replace('"', r"\"").replace("'", r"\'")
+        value = f'"{value}"'
+        return value
